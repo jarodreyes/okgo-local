9f6e34187b782c0464a5a038ef0dd99562bb3dda
Jarod Reyes <jarodreyes@gmail.com>
2018-10-07 12:28:21 -0700

    Playing notes over local machine

--- server.js
+++ server.js
@@ -4,55 +4,55 @@ var io = require('socket.io')(http);
 var midi = require('midi');
 
 // Create a new virtual midi controller called okgo-local
-var output = new midi.output();
-output.openVirtualPort('okgo-local');
+var midiOutput = new midi.output();
+midiOutput.openVirtualPort('okgo-local');
 
-// lookup for whatever OSC thing needs to be triggered based upon note sent to call.
-var notes = {
+// lookup for whatever Midi thing needs to be triggered based upon note sent to call.
+const playTime = 500; // milliseconds
+const notes = {
     "C2": "36",
-    "C3": "",
-    "C4": "",
-    "C5": "",
+    "C3": "48",
+    "C4": "60",
+    "C5": "72",
     "D2": "38",
-    "D3": "",
-    "D4": "",
-    "D5": "",
-    "Db2": "",
-    "Db3": "",
-    "Db4": "",
-    "Db5": "",
-    "E2": "",
-    "E3": "",
-    "E4": "",
-    "E5": "",
-    "E54": "",
-    "Eb2": "",
-    "Eb3": "",
-    "Eb4": "",
-    "Eb5": "",
-    "F2": "",
-    "F3": "",
-    "F4": "",
-    "F5": "",
-    "G2": "",
-    "G3": "",
-    "G4": "",
-    "Gb2": "",
-    "Gb3": "",
-    "Gb4": "",
-    "Gb5": "",
-    "A#2": "",
-    "A#3": "",
-    "A2": "",
-    "A3": "",
-    "A4": "",
-    "Ab2": "",
-    "Ab3": "",
-    "Ab4": "",
-    "B2": "",
-    "B3": "",
-    "B4": "",
-    "Bb4": ""
+    "D3": "50",
+    "D4": "62",
+    "D5": "74",
+    "Db2": "37",
+    "Db3": "49",
+    "Db4": "61",
+    "Db5": "73",
+    "E2": "40",
+    "E3": "52",
+    "E4": "64",
+    "E5": "76",
+    "Eb2": "39",
+    "Eb3": "51",
+    "Eb4": "63",
+    "Eb5": "75",
+    "F2": "41",
+    "F3": "53",
+    "F4": "65",
+    "F5": "77",
+    "G2": "43",
+    "G3": "55",
+    "G4": "67",
+    "Gb2": "42",
+    "Gb3": "54",
+    "Gb4": "66",
+    "Gb5": "78",
+    "A2": "45",
+    "A3": "57",
+    "A4": "69",
+    "Ab2": "44",
+    "Ab3": "56",
+    "Ab4": "68",
+    "B2": "47",
+    "B3": "59",
+    "B4": "71",
+    "Bb2": "46",
+    "Bb3": "58",
+    "Bb4": "70"
 };
 
 
@@ -63,25 +63,31 @@ app.get('/*', function (req, res) {
 // we should be able to support 1400-1800 concurrent connections (a lot more than 40)
 // https://stackoverflow.com/questions/15872788/maximum-concurrent-socket-io-connections
 io.on('connection', function (socket) {
-    console.log('a user connected');
+    console.log('a phone connected');
     socket.on('note', function (msg) {
 
         // I'm guessing this is the variable sent to OSC.
         // You can add it the value in the lookup above.
-        var playMe = notes[msg];
-        if(playMe === ""){
-            // do the OSC stuff here
-            console.log("playing note, ", msg);
-            // console.log('send this value ot OSC', playMe);
-        } else {
-            console.log("Invalid note: ", msg);
-        }
+        var midiNote = notes[msg];
+        sendMidi(midiNote);
 
     });
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 });
+
+const sendMidi = (note) => {
+    console.log(`Playing Note ${note}`);
+    midiOutput.sendMessage([144,note,100]);
+    midiOutput.sendMessage([176,66,0]); // sustain
+
+    // NOTE OFF
+    setTimeout(() => {
+      midiOutput.sendMessage([ 128, note, 0 ]);
+    }, playTime)
+
+}
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
